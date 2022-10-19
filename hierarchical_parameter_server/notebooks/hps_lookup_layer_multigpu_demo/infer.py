53fd23f4b21bf8417a4b2b5add0cba3dfdb4c905
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2022-10-19 13:20:52 +0000

    fix hps example: model cannot be saved then loaded

--- hierarchical_parameter_server/notebooks/hps_lookup_layer_multigpu_demo/infer.py
+++ hierarchical_parameter_server/notebooks/hps_lookup_layer_multigpu_demo/infer.py
@@ -47,14 +47,48 @@ def tf_dataset(cat_keys, dense_features, labels, batchsize):
     dataset = dataset.batch(batchsize, drop_remainder=True)
     return dataset
 
+# %%
+class InferenceModel(tf.keras.models.Model):
+    def __init__(self,
+                 slot_num,
+                 embed_vec_size,
+                 dense_dim,
+                 dense_model_path,
+                 **kwargs):
+        super(InferenceModel, self).__init__(**kwargs)
+        
+        self.embed_vec_size = embed_vec_size
+        self.slot_num = slot_num
+        self.dense_dim = dense_dim
+        self.lookup_layer = hps.LookupLayer(model_name="dnn", table_id=0, emb_vec_size=self.embed_vec_size, emb_vec_dtype=args["tf_vector_type"], name="lookup")
+        self.dense_model = tf.keras.models.load_model(dense_model_path)
+
+    def call(self, inputs):
+        input_cat = inputs[0]
+        input_dense = inputs[1]
+        
+        embedding_vector = self.lookup_layer(input_cat)
+        embedding_vector = tf.reshape(embedding_vector, shape=[-1, self.slot_num * self.embed_vec_size])
+        concat_feat = tf.concat([embedding_vector, input_dense], axis=1)
+        logit = self.dense_model(concat_feat)
+        return logit, embedding_vector
+
+    def summary(self):
+        inputs = [tf.keras.Input(shape=(self.slot_num, ), dtype=args["tf_key_type"]), 
+                  tf.keras.Input(shape=(self.dense_dim, ), dtype=tf.float32)]
+        model = tf.keras.models.Model(inputs=inputs, outputs=self.call(inputs))
+        return model.summary()
+
+# %%
 def inference_with_saved_model(args):
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         hps.Init(global_batch_size = args["global_batch_size"], ps_config_file = args["ps_config_file"])
-        model = tf.keras.models.load_model(args["saved_path"])
+        model = InferenceModel(args["slot_num"], args["embed_vec_size"], args["dense_dim"], args["dense_model_path"])
         model.summary()
     atexit.register(strategy._extended._collective_ops._pool.close) # type: ignore
 
+    @tf.function
     def _infer_step(inputs, labels):
         logit, embeddings = model(inputs)
         return logit, embeddings
@@ -81,5 +115,5 @@ def inference_with_saved_model(args):
 # %%
 embeddings_peek, inputs_peek = inference_with_saved_model(args)
 
-print(inputs_peek[-1][0].values)
+print(inputs_peek[-1])
 print(embeddings_peek[-1])
