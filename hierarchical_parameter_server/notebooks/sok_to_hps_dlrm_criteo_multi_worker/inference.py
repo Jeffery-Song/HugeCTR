b96a69f93b593ee987c731e5b02a0019f4f1ee72
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2022-11-05 08:59:22 +0000

    amend

--- hierarchical_parameter_server/notebooks/sok_to_hps_dlrm_criteo_multi_worker/inference.py
+++ hierarchical_parameter_server/notebooks/sok_to_hps_dlrm_criteo_multi_worker/inference.py
@@ -2,6 +2,10 @@ import time
 import os
 # this must be set before hps is imported
 os.environ["COLL_NUM_REPLICA"] = "8"
+proxy = None
+if "http_proxy" in os.environ:
+    proxy = os.environ["http_proxy"]
+    del os.environ["http_proxy"]
 import hierarchical_parameter_server as hps
 import numpy as np
 import tensorflow as tf
@@ -13,7 +17,7 @@ from numba import njit
 
 args = dict()
 args["gpu_num"] = 8                               # the number of available GPUs
-args["iter_num"] = 300                           # the number of training iteration
+args["iter_num"] = 6000                           # the number of training iteration
 args["slot_num"] = 26                             # the number of feature fields in this embedding layer
 args["embed_vec_size"] = 128                      # the dimension of embedding vectors
 args["dense_dim"] = 13                            # the dimension of dense features
@@ -27,7 +31,7 @@ args["np_vector_type"] = np.float32
 args["tf_key_type"] = tf.int32
 args["tf_vector_type"] = tf.float32
 args["optimizer"] = "plugin_adam"
-args["dataset_path"] = "saved_dataset_small"
+args["dataset_path"] = "saved_dataset"
 
 # load vocabulary_range_per_slot from yaml file
 file = open('criteo_full.yaml', 'r', encoding="utf-8")
@@ -112,71 +116,49 @@ def inference_with_saved_model(args):
     embeddings_peek = list()
     inputs_peek = list()
 
-    def _dataset_fn(input_context):
-        replica_batch_size = input_context.get_per_replica_batch_size(args["global_batch_size"])
+    def _dataset_fn(num_replica, local_id):
+        assert(args["global_batch_size"] % num_replica == 0)
+        replica_batch_size = args["global_batch_size"] // num_replica
         dataset = tf.data.experimental.load(args["dataset_path"], compression="GZIP")
-        dataset = dataset.shard(input_context.num_input_pipelines, input_context.input_pipeline_id)
+        dataset = dataset.shard(num_replica, local_id)
         if dataset.element_spec[0].shape[0] != replica_batch_size:
             print("loaded dataset has batch size {}, but we need {}, so we have to rebatch it!".format(dataset.element_spec[0].shape[0], replica_batch_size))
             dataset = dataset.unbatch().batch(replica_batch_size, num_parallel_calls=56)
         else:
             print("loaded dataset has batch size we need, so directly use it")
         dataset = dataset.cache()
-        dataset = dataset.prefetch(24)
+        dataset = dataset.prefetch(1000)
         return dataset
-
-    # # with strategy.scope():
-    dataset = strategy.distribute_datasets_from_function(_dataset_fn
-    # , tf.distribute.InputOptions(
-    #     experimental_fetch_to_device=True,
-    #     experimental_replication_mode=tf.distribute.InputReplicationMode.PER_WORKER,
-    #     experimental_place_dataset_on_device=False,
-    #     experimental_per_replica_buffer_size=8
-    # )
-    )
-
-    for _ in tqdm(dataset, "warm dataset"):
-        pass
-    for _ in tqdm(dataset, "dataset shoulded be warm"):
-        pass
-    ds_time = 0
-    md_time = 0
-    dataset_iter = iter(dataset)
+    dataset = _dataset_fn(args["gpu_num"], int(os.environ["HPS_WORKER_ID"]))
 
     ret_list = []
     for sparse_keys, dense_features, labels in tqdm(dataset, "warmup run"):
         inputs = [sparse_keys, dense_features]
         ret = strategy.run(_warmup_step, args=(inputs, labels))
-    for sparse_keys, dense_features, labels in tqdm(dataset, "warmup run"):
-        inputs = [sparse_keys, dense_features]
-        ret = strategy.run(_warmup_step, args=(inputs, labels))
-        # ret_list.append(ret)
-    # for i in tqdm(ret_list, "warmup should be done"):
-    #     ret = strategy.run(_warmup_step, args=i)
-    # for i in tqdm(ret_list, "warmup should be done"):
-    #     ret = strategy.run(_warmup_step, args=i)
+        ret_list.append(ret)
+    for i in tqdm(ret_list, "warmup should be done"):
+        ret = strategy.run(_warmup_step, args=i)
+    for i in tqdm(ret_list, "warmup should be done"):
+        ret = strategy.run(_warmup_step, args=i)
 
+    ds_time = 0
+    md_time = 0
+    os.environ["http_proxy"] = proxy
     for i in range(args["iter_num"]):
         t0 = time.time()
-        sparse_keys, dense_features, labels = next(dataset_iter)
         t1 = time.time()
-        inputs = [sparse_keys, dense_features]
-        # print(sparse_keys)
-        # logits = strategy.run(_infer_step, args=(inputs, labels))
-        logits = _whole_infer_step((inputs, labels))
-        # logits = _whole_infer_step(ret_list[i])
+        logits = _whole_infer_step(ret_list[i])
         t2 = time.time()
         ds_time += t1 - t0
         md_time += t2 - t1
-        if i % 10 == 0:
-            print(i, "time {:.6} {:.6}".format(ds_time / 10, md_time / 10))
+        if i % 100 == 0:
+            print(i, "time {:.6} {:.6}".format(ds_time / 100, md_time / 100))
             ds_time = 0
             md_time = 0
     return embeddings_peek, inputs_peek
 
 def proc_func(id):
     print(f"worker {id} at process {os.getpid()}")
-    # time.sleep(30)
     os.environ["TF_CONFIG"] = '{"cluster": {"worker": ["localhost:12340", "localhost:12341", "localhost:12342", "localhost:12343", "localhost:12344", "localhost:12345", "localhost:12346", "localhost:12347"]}, "task": {"type": "worker", "index": ' + str(id) + '} }'
     tf.config.set_visible_devices(tf.config.list_physical_devices('GPU')[i], 'GPU')
     os.environ["HPS_WORKER_ID"] = str(id)
